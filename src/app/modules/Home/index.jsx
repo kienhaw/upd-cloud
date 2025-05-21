83f91f89958de1997b5554a45fc24e1001220879
Lee Kien Haw <kien.lee@chubb.com>
2025-05-21 14:26:20 +0800

    Added contact us section

--- src/app/modules/Home/index.jsx
+++ src/app/modules/Home/index.jsx
@@ -12,6 +12,14 @@ import {
 } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import LogoImg from "../../../assets/img/logo.png";
+import FooterLogoImg from "../../../assets/img/footer-logo.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import { faLocationDot, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import BusinessInqImg from '../../../assets/img/business-inquiries.png';
+import CareerOppImg from '../../../assets/img/career-opportunities.png';
+import ProductInfoImg from '../../../assets/img/product-info.png';
+import ContactUsImg from '../../../assets/img/contactus.png';
 
 export default (props) => {
   const {
@@ -37,7 +45,7 @@ export default (props) => {
           <Navbar.Brand href="#home">
             <img
               src={LogoImg}
-              width="100%"
+              width="75px"
               className="d-inline-block align-top"
               alt="React Bootstrap logo"
             />
@@ -57,54 +65,75 @@ export default (props) => {
       </Navbar>
 
       {/* body */}
-      <Container fluid={"md"} className="my-3" style={{ minHeight: "55vh" }}>
-        <section id="inquiry" className="mt-5">
-          <Row>
-            <Col>
-              <h2 className="text-primary">
+
+      {/* Contact section */}
+      {/* height should be depends on screensize */}
+      <section id="contactus">
+        <Container style={{display: 'flex', alignItems: 'center', height: '55vh'}}>
+          <Row className="w-100 gx-5">
+            <Col xs={12} sm={7} className="mt-5">
+            <div style={{width: '85%'}}>
+            <h2 className="text-primary fw-bold mb-4 fs-1">Contact Us</h2>
+            <p className="">
+            Fill out the form below to consult with us and begin your exclusive cloud
+journey! If you have any questions, feel free to check our FAQ or contact
+us directly—our specialists will be happy to assist you.
+            </p>
+            </div>
+            
+            </Col>
+            <Col xs={12} sm={4}>
+              <img src={ContactUsImg} alt="" className="w-100" />
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      {/* Inquiry section */}
+      <section id="inquiry" className="py-4">
+        <Container fluid={"md"} className="my-3" style={{ minHeight: "55vh" }}>
+          <Row className="gx-5">
+            <Col xs={12} sm={5} className="mt-4">
+              <h3 className="text-primary mb-4 fw-bold">
                 Welcome to reach out through the following channels
-              </h2>
-              <Stack direction="horizontal" gap={5} className="mb-2">
-                <i>Icon</i>
-                <div>
-                  <p>
-                    <b>Product Information</b>
-                  </p>
-                  <p>
-                    Click now to explore the products and services offered by
-                    UDP Cloud
-                  </p>
-                  <p>Learn More</p>
-                </div>
-              </Stack>
-              <Stack direction="horizontal" gap={5} className="mb-2">
-                <i>Icon</i>
-                <div>
-                  <p>
-                    <b>Career Opportunities</b>
-                  </p>
-                  <p>
-                    Join the UDP CLOUD family! Click to discover available
-                    positions.
-                  </p>
-                  <p>Join Us</p>
-                </div>
-              </Stack>
-              <Stack direction="horizontal" gap={5} className="mb-2">
-                <i>Icon</i>
-                <div>
-                  <p>
-                    <b>Business Inquiries</b>
-                  </p>
-                  <p>Contact our sales team to learn more about UDP CLOUD.</p>
-                  <p>sales@udpcloud.com.my</p>
-                </div>
+              </h3>
+              <Stack direction="vertical" gap={3}>
+                <Stack direction="horizontal" gap={5} >
+                  <img src={ProductInfoImg} width={"75px"} alt="Product Information"/>
+                  <div>
+                    <h4 className="fw-bold mb-1">Product Information</h4>
+                    <p className="mb-1">
+                      Click now to explore the products and services offered by
+                      UDP Cloud
+                    </p>
+                    <p className="text-primary mb-0">Learn More</p>
+                  </div>
+                </Stack>
+                <Stack direction="horizontal" gap={5} >
+                  <img src={CareerOppImg} width={"75px"} alt="career opportunities"/>
+                  <div>
+                    <h4 className="fw-bold mb-1">Career Opportunities</h4>
+                    <p className="mb-1">
+                      Join the UDP CLOUD family! Click to discover available
+                      positions.
+                    </p>
+                    <p className="text-primary mb-0">Join Us</p>
+                  </div>
+                </Stack>
+                <Stack direction="horizontal" gap={5}>
+                  <img src={BusinessInqImg} width={"75px"} alt="Business Inquiries"/>
+                  <div>
+                    <h4 className="fw-bold mb-1">Business Inquiries</h4>
+                    <p className="mb-1">Contact our sales team to learn more about UDP CLOUD.</p>
+                    <a href="mailto:sales@udpcloud.com.my" className="text-primary text-decoration-none">sales@udpcloud.com.my</a>
+                  </div>
+                </Stack>
               </Stack>
             </Col>
-            <Col>
-              <Card bg={"light"} key={"Light"} text={"dark"} className="mb-2">
-                <Card.Body>
-                  <Card.Title>
+            <Col xs={12} sm={7}>
+              <Card bg={"light"} key={"Light"} text={"dark"} className="mb-2" style={{'borderRadius': '30px', 'border': 'none', 'boxShadow': 'rgb(42, 190, 209) 3px 6px 0px 2px'}}>
+                <Card.Body className="bg-white" style={{'borderRadius': '30px', padding: '35px 45px'}}>
+                  <Card.Title className="mb-3 fw-bold">
                     Got questions? We'd love to hear from you!{" "}
                   </Card.Title>
                   <Form onSubmit={handleSubmit(onSubmit)}>
@@ -122,6 +151,23 @@ export default (props) => {
                       />
                       <Form.Control.Feedback type="invalid">
                         {errors.firstName?.message}
+                      </Form.Control.Feedback>
+                    </Form.Group>
+
+                    <Form.Group
+                      className="mb-3"
+                      controlId="exampleForm.ControlInput5"
+                    >
+                      <Form.Control
+                        type="text"
+                        placeholder="Last name"
+                        {...register("lastName", {
+                          required: "Last name is required",
+                        })}
+                        isInvalid={!!errors.lastName}
+                      />
+                      <Form.Control.Feedback type="invalid">
+                        {errors.lastName?.message}
                       </Form.Control.Feedback>
                     </Form.Group>
 
@@ -180,13 +226,13 @@ export default (props) => {
                     >
                       <Form.Control
                         as="textarea"
-                        rows={3}
+                        rows={5}
                         placeholder="Please describe your question in detail.
-  We will respond to you as soon as possible."
+We will respond to you as soon as possible."
                       />
                     </Form.Group>
 
-                    <Button variant="primary" type="submit">
+                    <Button className="text-white" variant="primary" type="submit" size="sm" style={{width: '100px', borderRadius: '30px', fontSize: '16px'}}>
                       Submit
                     </Button>
                   </Form>
@@ -194,61 +240,69 @@ export default (props) => {
               </Card>
             </Col>
           </Row>
-        </section>
-      </Container>
+        </Container>
+      </section>
 
       {/* footer */}
-      <Container>
-        <Row>
-          <Col xs={6}>
-            <img src={LogoImg} width={"50%"} />
-            <div>
-              <b>Visit us online at www.udpcloud.com.my</b>
-            </div>
-          </Col>
-          <Col xs={3}>
-            <h3 className="text-primary">Quick Links</h3>
-            <p>Cloud Products & Services</p>
-            <p>About Us</p>
-            <p>Contact Us</p>
-          </Col>
-          <Col xs={3}>
-            <h3 className="text-primary">Get in Touch</h3>
-            <Stack direction="horizontal" gap={2}>
-              <i>icon</i>
-              <span>Tel: +603 5590 9999</span>
-            </Stack>
-            <Stack direction="horizontal" gap={2}>
-              <i className="align-self-start">locs</i>
-              <div>
-                <p className="mb-0">
-                  <b>Level 10,Boutique Office 3</b>
-                </p>
-                <p className="mb-0">B03-D (PILLAR 7) KL Eco City</p>
-                <p className="mb-0">59200 Kuala Lumpur</p>
+      <footer>
+        <Container className="my-5">
+          <Row>
+            <Col xs={6} className="mt-2">
+              <img src={FooterLogoImg} width={"30%"} />
+              <div className="mt-3">
+                Visit us online at <b>www.udpcloud.com.my</b>
               </div>
-            </Stack>
-            <Stack direction="horizontal" gap={2}>
-              <i className="align-self-start">emai</i>
-              <a
-                href="mailto:sales@udpcloud.com.my"
-                className="text-dark"
-                style={{ textDecoration: "none", fontWeight: "bold" }}
-              >
-                sales@udpcloud.com.my
-              </a>
-            </Stack>
-          </Col>
-        </Row>
-        <Row className="my-3">
-          <Col>
-            <hr
-              className="bg-primary border-primary"
-              style={{ height: "3px" }}
-            />
-          </Col>
-        </Row>
-      </Container>
+            </Col>
+            <Col xs={3}>
+              <h4 className="text-primary mb-4 fw-bold">Quick Links</h4>
+              <p>Cloud Products & Services</p>
+              <p>About Us</p>
+              <p>Contact Us</p>
+            </Col>
+            <Col xs={3}>
+              <h4 className="text-primary mb-4 fw-bold">Get in Touch</h4>
+              <Stack direction="horizontal" gap={2}>
+                <div style={{'width': '15px'}}>
+                  <FontAwesomeIcon icon={faWhatsapp} className="text-primary" />
+                </div>
+                <span>Tel: <b>+603 5590 9999</b></span>
+              </Stack>
+              <Stack direction="horizontal" gap={2}>
+                <div className="align-self-start" style={{'width': '15px'}}>
+                  <FontAwesomeIcon icon={faLocationDot} className="text-primary" />
+                </div>
+                <div className="">
+                  <p className="mb-0">
+                    <b>Level 10,Boutique Office 3</b>
+                  </p>
+                  <p className="mb-0">B03-D (PILLAR 7) KL Eco City</p>
+                  <p className="mb-0">59200 Kuala Lumpur</p>
+                </div>
+              </Stack>
+              <Stack direction="horizontal" gap={2}>
+                <div style={{'width': '15px'}}>
+                  <FontAwesomeIcon icon={faEnvelope} className="text-primary" />
+                </div>
+                <a
+                  href="mailto:sales@udpcloud.com.my"
+                  className="text-dark"
+                  style={{ textDecoration: "none" }}
+                >
+                  sales@udpcloud.com.my
+                </a>
+              </Stack>
+            </Col>
+          </Row>
+          <Row className="my-3">
+            <Col>
+              <hr
+                className="bg-primary border-primary"
+                style={{ height: "3px" }}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </footer>
     </>
   );
 };
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,6 +1,6 @@
 import { applyMiddleware } from "redux";
 import { configureStore } from '@reduxjs/toolkit';
-import { composeWithDevTools } from "redux-devtools-extension";
+import { composeWithDevTools } from "@redux-devtools/extension";
 import {thunk} from "redux-thunk";
 
 import rootReducer from "../reducers/root.reducer";
